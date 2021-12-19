4909efefc80201e8348897a9d6f4813389b5ceef
haythem achour <93125352+Haythem1997@users.noreply.github.com>
2021-12-19 15:45:07 +0100

    Add files via upload

---
+++ src/component/Profil/Address.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Address() {
+    return (
+        <div>
+            My Address:
+              Sousse-kalaa kebira-City nozha 
+        </div>
+    )
+}
+
+export default Address
---
+++ src/component/Profil/FullName.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FullName() {
+    return (
+        <div>
+          My Name is:
+           haythem achour
+        </div>
+    )
+}
+
+export default FullName
+
+
